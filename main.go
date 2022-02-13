e55b9593b8022698dc2b79d79e335d65bac45eb1
TH3_S1LENC3 <54558771+Th3-S1lenc3@users.noreply.github.com>
2022-02-13 22:00:11 +0000

    Add output controls

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
   "flag"
   "log"
   "fmt"
+  "os"
 )
 
 func main() {
@@ -13,7 +14,10 @@ func main() {
   }
 
   morseString := flag.String("m", "", "Morse code string to convert.")
-  appDir := flag.String("appDir", "", "Path to app config dir, if part of larger application")
+  appDir := flag.String("appDir", "", "Path to app config dir, if part of larger application.")
+  outputDir := flag.String("o", "", "Path to write output file.")
+  override := flag.Bool("override", false, "Override existing JSON file.")
+  indent := flag.Bool("indent", false, "Indent output JSON.")
 
   flag.Parse()
 
@@ -27,7 +31,20 @@ func main() {
     log.Fatal(err)
   }
 
-  err = convert.WriteNotesToFile("", true, true)
+  var outputPath string
+
+  if *outputDir == "" {
+    cwd, err := os.Getwd()
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    outputPath = fmt.Sprintf("%s/note.json", cwd)
+  } else {
+    outputPath = fmt.Sprintf("%s/note.json", *outputDir)
+  }
+
+  err = convert.WriteNotesToFile(outputPath, *override, *indent)
   if err != nil {
     log.Fatal(err)
   }
